010350d54970b152fcb10776b7b42e6384c19b86
Roman <horoshailo.r@gmail.com>
2021-06-25 16:26:02 +0300

    refactor: change dependencies (argon2) and db url connection

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use('/api', routes);
 
 app.listen(3000, async () => {
   console.log('Server ready!')
-  const mongoConnection = await mongoose.connect('mongodb://localhost/nodejs-auth');
+  const mongoConnection = await mongoose.connect('mongodb+srv://r_horoshailo:tapi1979Cosh!%40%23@cluster0.ztjex.mongodb.net/test', {useNewUrlParser: true});
   console.log('Database ready!')
   
   console.log('Listening on port 3000')
